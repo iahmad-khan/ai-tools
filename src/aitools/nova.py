cfa59037704d7ba3405e361746cf1cb9669e83b9
Nacho Barrientos <nacho.barrientos@cern.ch>
2013-10-24 20:54:56 +0200

    Add nova.delete

--- src/aitools/nova.py
+++ src/aitools/nova.py
@@ -23,16 +23,12 @@ class NovaClient():
             key_name=None, availability_zone=None):
         vmname = self.__vmname_from_fqdn(fqdn)
         logging.info("Creating virtual machine '%s'..." % vmname)
-        tenant = client.Client(self.username, self.password,
-            self.tenant_name, 
-            auth_url=self.auth_url,
-            cacert=CERN_CA_BUNDLE,
-            service_type="compute",
-            timeout=self.timeout)
-
+        tenant = self.__init_client()
         try:
             flavor_id = self.__resolve_id(tenant.flavors.list(), flavor)
+            logging.debug("Flavor '%s' has id '%s'" % (flavor, flavor_id))
             image_id = self.__resolve_id(tenant.images.list(), image)
+            logging.debug("Image '%s' has id '%s'" % (image, image_id))
             if not self.dryrun:
                 tenant.servers.create(name=vmname,
                     image=image_id,
@@ -43,6 +39,36 @@ class NovaClient():
             else:
                 logging.info("VM '%s' not created because dryrun is enabled" % vmname)
                 
+        except novaclient.exceptions.ClientException, error:
+            raise AiToolsNovaError(error)
+        except novaclient.exceptions.ConnectionRefused, error:
+            raise AiToolsNovaError(error)
+
+    def delete(self, fqdn):
+        vmname = self.__vmname_from_fqdn(fqdn)
+        logging.info("Deleting virtual machine '%s'..." % vmname)
+        tenant = self.__init_client()
+        try:
+            server_id = self.__resolve_id(tenant.servers.list(), vmname)
+            logging.debug("Server '%s' has id '%s'" % (vmname, server_id))
+            if not self.dryrun:
+                tenant.servers.get(server_id).delete()
+                logging.info("Delete request sent")
+            else:
+                logging.info("VM '%s' not deleted because dryrun is enabled" % vmname)
+        except novaclient.exceptions.ClientException, error:
+            raise AiToolsNovaError(error)
+        except novaclient.exceptions.ConnectionRefused, error:
+            raise AiToolsNovaError(error)
+
+    def __init_client(self):
+        try:
+            return client.Client(self.username, self.password,
+                    self.tenant_name,
+                    auth_url=self.auth_url,
+                    cacert=CERN_CA_BUNDLE,
+                    service_type="compute",
+                    timeout=self.timeout)
         except novaclient.exceptions.ClientException, error:
             raise AiToolsNovaError(error)
         except novaclient.exceptions.ConnectionRefused, error:
