7674ff2ba14a189e824aeff1e8dc68d0a88bae2b
Nacho Barrientos <nacho.barrientos@cern.ch>
2013-10-25 16:33:38 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ai-tools',
+      version='5.0',
+      description='Tools for Agile Infrastructure project',
+      author='Nacho Barrientos',
+      author_email='ai-config-team@cern.ch',
+      url='http://www.cern.ch/ai',
+      package_dir= {'': 'src'},
+      packages=['aitools'],
+      scripts=['bin/ai-bs-vm', 'bin/ai-kill-vm'],
+     )
