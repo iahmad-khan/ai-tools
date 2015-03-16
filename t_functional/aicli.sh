a468952e9f21dbce2b2eda8e91ec1d73486b3984
Nacho Barrientos <nacho.barrientos@cern.ch>
2015-03-16 14:58:49 +0100

    Rename ai-cli to ai-foreman

--- t_functional/aicli.sh
+++ t_functional/aicli.sh
@@ -14,17 +14,17 @@ IN=$(mktemp)
 echo "aifcliftest10.cern.ch 192.168.0.2 aa:bb:cc:dd:ee:FF" > $IN
 echo "aifcliftest11.cern.ch 192.168.0.3 aa-bb-cc-dd-ee-FE" >> $IN
 echo "aifcliftest12.cern.ch 192.168.0.4 AA:bb:cc:dd:ee:FD" >> $IN
-cat $IN | ai-cli --config $CONF addhost -c playground/ibarrien/test1 -e qa -a x86_64 -p "Kickstart default" -o "SLC 6.6" -r
+cat $IN | ai-foreman --config $CONF addhost -c playground/ibarrien/test1 -e qa -a x86_64 -p "Kickstart default" -o "SLC 6.6" -r
 echo "Showhost..."
-ai-cli --config $CONF showhost aifcliftest10.cern.ch
-ai-cli --config $CONF -g playground/ibarrien/test1 showhost aifcliftest10.cern.ch
-ai-cli --config $CONF -g playground/ibarrien/test1 showhost
+ai-foreman --config $CONF showhost aifcliftest10.cern.ch
+ai-foreman --config $CONF -g playground/ibarrien/test1 showhost aifcliftest10.cern.ch
+ai-foreman --config $CONF -g playground/ibarrien/test1 showhost
 echo "Updatehost..."
-ai-cli --config $CONF updatehost -e production aifcliftest10.cern.ch -y
-ai-cli --config $CONF updatehost -o "CentOS 7.0" aifcliftest11.cern.ch -y
-ai-cli --config $CONF -g playground/ibarrien/test1 updatehost -e production -c playground/ibarrien/test2 -y
-ai-cli --config $CONF -g playground/ibarrien/test2 showhost
+ai-foreman --config $CONF updatehost -e production aifcliftest10.cern.ch -y
+ai-foreman --config $CONF updatehost -o "CentOS 7.0" aifcliftest11.cern.ch -y
+ai-foreman --config $CONF -g playground/ibarrien/test1 updatehost -e production -c playground/ibarrien/test2 -y
+ai-foreman --config $CONF -g playground/ibarrien/test2 showhost
 echo "Delhost..."
-ai-cli --config $CONF delhost -y aifcliftest10.cern.ch
-ai-cli --config $CONF -g playground/ibarrien/test2 delhost -y
+ai-foreman --config $CONF delhost -y aifcliftest10.cern.ch
+ai-foreman --config $CONF -g playground/ibarrien/test2 delhost -y
 rm $IN $CONF
