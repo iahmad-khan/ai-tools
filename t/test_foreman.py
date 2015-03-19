c2c181ef6baacad55dd9701983cd525d79bea8b7
Nacho Barrientos <nacho.barrientos@cern.ch>
2015-03-19 12:37:54 +0100

    Add unit test

--- t/test_foreman.py
+++ t/test_foreman.py
@@ -324,6 +324,18 @@ class TestForemanClient(unittest.TestCase):
             .assert_called_once_with('put', full_uri("hosts/foo.cern.ch"),
                 ANY, json.dumps(expected_payload))
 
+    @patch.object(HTTPClient, 'do_request',
+        return_value=generate_response(requests.codes.UNPROCESSABLE_ENTITY,
+            {'error': {'full_messages': ['MAC not valid']}}))
+    def test_updatehost_bad_data(self, *args):
+        self.assertRaises(AiToolsForemanError, self.client.updatehost,
+            fqdn='foo.cern.ch', mac='foo1')
+        expected_payload = {'mac': 'foo1'}
+        super(ForemanClient, self.client).do_request\
+            .assert_called_once_with('put', full_uri("hosts/foo.cern.ch"),
+                ANY, json.dumps(expected_payload))
+
+
     #### GETHOST ####
 
     @patch.object(ForemanClient, '_ForemanClient__resolve_model',
