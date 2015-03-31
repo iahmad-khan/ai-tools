1833c81aaeb1df4bf905f8202773d23373494724
Akos Hencz <akos.hencz@cern.ch>
2015-03-31 14:13:14 +0200

    Add Pwn library and tests

---
+++ src/aitools/pwn.py
@@ -0,0 +1,173 @@
+__author__ = 'ahencz'
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import requests
+import logging
+import re
+from aitools.errors import AiToolsHTTPClientError
+from aitools.errors import AiToolsPwnNotFoundError
+from aitools.errors import AiToolsPwnError
+from aitools.errors import AiToolsPwnNotAllowedError
+from aitools.errors import AiToolsPwnInternalServerError
+from aitools.errors import AiToolsPwnNotImplementedError
+from aitools.httpclient import HTTPClient
+from aitools.config import PwnConfig
+
+logger = logging.getLogger(__name__)
+
+class PwnClient(HTTPClient):
+
+    def __init__(self, host=None, port=None, timeout=None, show_url=False, dryrun=False, deref_alias=False):
+        """
+        Pwn client for interacting with the Pwn service. Autoconfigures via the AiConfig
+        object.
+
+        :param host: override the auto-configured Pwn host
+        :param port: override the auto-configured Pwn port
+        :param timeout: override the auto-configured Pwn timeout
+        :param show_url: print the URLs used to sys.stdout
+        :param dryrun: create a dummy client
+        :param deref_alias: dereference dns load balanced aliases
+        """
+        pwn = PwnConfig()
+        self.host = host or pwn.pwn_hostname
+        self.port = int(port or pwn.pwn_port)
+        self.timeout = int(timeout or pwn.pwn_timeout)
+        self.dryrun = dryrun
+        self.show_url = show_url
+        self.deref_alias = deref_alias
+
+    def fetch_pwn_endpoint(self, entity=None, scope=None):
+        if scope and not entity:
+            if scope == 'module':
+                pwn_endpoint = "pwn/v1/module/"
+            elif scope == 'hostgroup':
+                pwn_endpoint = "pwn/v1/hostgroup/"
+            else:
+                raise AttributeError("scope must be either 'module' or 'hostgroup'")
+        elif scope and entity:
+            if scope == 'module':
+                pwn_endpoint = "pwn/v1/module/%s/" % entity
+            elif scope == 'hostgroup':
+                pwn_endpoint = "pwn/v1/hostgroup/%s/" % entity.replace("/", "-")
+            else:
+                raise AttributeError("scope must be either 'module' or 'hostgroup'")
+        else:
+            raise AttributeError("scope must be provided")
+        return pwn_endpoint
+
+    def clean_owners(self, owners):
+        """
+        Make the owners into a list (if it was a single string)
+        Strip @cern.ch from the owners list
+        """
+        if isinstance(owners, str):
+            mylist = [ owners ]
+        elif isinstance(owners, list):
+            mylist = owners
+        else:
+            raise AttributeError("owners must be either a string or a list of strings")
+        return map( lambda owner: owner.lower().strip().replace('@cern.ch',''), mylist )
+
+    def get_ownership(self, entity, scope):
+        pwn_endpoint = self.fetch_pwn_endpoint(entity, scope)
+        (code, body) = self.__do_api_request("get", pwn_endpoint)
+        if code == requests.codes.not_found:
+            raise AiToolsPwnNotFoundError("%s %s not found in Pwn" % (scope.title(), entity))
+        return body
+
+    def update_or_create_ownership(self, entity, scope, owners, options=None, **kwargs):
+        try:
+            self.get_ownership(entity, scope)
+        except AiToolsPwnNotFoundError:
+            return self.create_ownership(entity, scope, owners, options=options, **kwargs)
+        else:
+            return self.put_ownership(entity, scope, owners, options=options, **kwargs)
+
+    def create_ownership(self, entity, scope, owners, options=None, **kwargs):
+        if self.dryrun:
+            logger.info("Not adding '%s' to Pwn as dryrun enabled" % entity)
+            return True
+        logger.info("Adding ownership of '%s' to Pwn" % entity)
+        pwn_endpoint = self.fetch_pwn_endpoint(scope=scope)
+        data = dict()
+        data[scope] = entity.replace("/", "-")
+        data["owners"] = self.clean_owners(owners)
+        if options:
+            data["options"] = options
+        d = json.dumps(data)
+        (code, body) = self.__do_api_request("post", pwn_endpoint, data=d)
+        if code == requests.codes.not_found:
+            raise AiToolsPwnNotFoundError("Ownership endpoint '%s' not found in Pwn" % pwn_endpoint)
+        elif code == requests.codes.not_allowed:
+            raise AiToolsPwnNotAllowedError("Not allowed to post '%s' to '%s'" % (entity, pwn_endpoint))
+        elif code == requests.codes.not_implemented:
+            raise AiToolsPwnNotImplementedError("Not implemented trying to post to '%s'" % pwn_endpoint)
+        elif code == requests.codes.internal_server_error:
+            raise AiToolsPwnInternalServerError("Received 500 trying to post to '%s'" % pwn_endpoint)
+        return body
+
+    def put_ownership(self, entity, scope, owners, options=None, **kwargs):
+        if self.dryrun:
+            logger.info("Not adding '%s' to Pwn as dryrun enabled" % entity)
+            return True
+        pwn_endpoint = self.fetch_pwn_endpoint(entity, scope)
+        data = dict()
+        data[scope] = entity.replace("/", "-")
+        data["owners"] = self.clean_owners(owners)
+        if options:
+            data["options"] = options
+        d = json.dumps(data)
+        (code, body) = self.__do_api_request("put", pwn_endpoint, data=d)
+        if code == requests.codes.not_found:
+            raise AiToolsPwnNotFoundError("%s %s not found in Pwn" % (scope.title(), entity))
+        elif code == requests.codes.not_allowed:
+            raise AiToolsPwnNotAllowedError("Not allowed to put details for %s in Pwn" % entity)
+        elif code == requests.codes.not_implemented:
+            raise AiToolsPwnNotImplementedError("Not implemented when trying to put to %s" % pwn_endpoint)
+        elif code == requests.codes.internal_server_error:
+            raise AiToolsPwnInternalServerError("Received 500 when trying to put to %s" % pwn_endpoint)
+        return body
+
+    def delete_ownership(self, entity, scope):
+        if self.dryrun:
+            logger.info("Not deleting '%s' from Pwn as dryrun selected" % entity)
+            return True
+        pwn_endpoint = self.fetch_pwn_endpoint(entity, scope)
+        (code, body) = self.__do_api_request("delete", pwn_endpoint)
+        if code == requests.codes.not_found:
+            raise AiToolsPwnNotFoundError("%s %s not found, can't delete" % (scope.title(), entity))
+        elif code == requests.codes.not_allowed:
+            raise AiToolsPwnNotAllowedError("Not allowed to delete %s '%s'" % (scope.title(), entity))
+        elif code == requests.codes.not_implemented:
+            raise AiToolsPwnNotImplementedError("Not implemented trying to delete at '%s'" % pwn_endpoint)
+        elif code == requests.codes.internal_server_error:
+            raise AiToolsPwnInternalServerError("Received a 500 trying to delete at '%s'" % pwn_endpoint)
+        return body
+
+    def __do_api_request(self, method, url, data=None):
+        url="https://%s:%u/%s" % \
+            (self.host, self.port, url)
+        if self.deref_alias:
+            url = deref_url(url)
+        headers = {'Accept': 'application/json',
+                   'Accept-Encoding': 'deflate'}
+        if data:
+            headers["Content-Type"] = "application/json"
+
+        if self.show_url:
+            print url
+        try:
+            code, response = super(PwnClient, self).do_request(method, url, headers, data)
+            body = response.text
+            if code == requests.codes.unauthorized or code == requests.codes.forbidden:
+                raise AiToolsPwnNotAllowedError("Unauthorized trying '%s' at '%s'" % (method, url))
+            if code == requests.codes.ok:
+                if re.match('application/json', response.headers['content-type']):
+                    body = response.json()
+            return (code, body)
+        except AiToolsHTTPClientError, error:
+            raise AiToolsPwnError(error)
---
+++ t/test_pwn.py
@@ -0,0 +1,118 @@
+__author__ = "ahencz"
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../src"))
+import unittest
+from argparse import ArgumentParser
+from aitools.config import PwnConfig
+from aitools.pwn import PwnClient
+from aitools.errors import AiToolsPwnNotAllowedError
+from aitools.errors import AiToolsPwnNotFoundError
+from aitools.errors import AiToolsPwnNotImplementedError
+
+# Test cases were based on Tbag tests
+
+class TestPwn(unittest.TestCase):
+
+    def setUp(self):
+        self.pwn_config = PwnConfig(configfile=os.path.abspath("ai.conf"))
+        parser = ArgumentParser()
+        self.pwn_config.add_standard_args(parser)
+        (pargs, _) = self.pwn_config.parser.parse_known_args()
+        self.pwn_config.read_config_and_override_with_pargs(pargs)
+        self.test_entities = { "hostgroup": "punch/puppetdb", "module": "my_module" }
+        self.test_ownership = [["ahencz@cerN.ch ", "ai-config-team"], ["ai-robots@CERN.CH"]]
+        self.test_options = { "hasPhysical": True }
+
+# yes, explicit order is frowned upon, but add/show/delete so...
+    def test_00_default_args(self):
+        self.assertEqual(self.pwn_config.pwn_hostname, "teigitest.cern.ch")
+
+    def test_01_pwn_add_hostgroup_ownership(self):
+        pwn = PwnClient()
+        scope = 'hostgroup'
+        entity = self.test_entities[scope]
+        owners = self.test_ownership[0]
+        options = self.test_options
+        pwn.update_or_create_ownership(entity, scope, owners, options=options)
+
+    def test_02_pwn_get_hostgroup_ownership(self):
+        pwn = PwnClient()
+        scope = 'hostgroup'
+        entity = self.test_entities[scope]
+        expected_owners = pwn.clean_owners(self.test_ownership[0])
+        expected_options = self.test_options
+        result = pwn.get_ownership(entity, scope)
+        self.assertEqual(expected_owners, result['owners'])
+        self.assertEqual(expected_options, result['options'])
+
+    def test_03_pwn_update_hostgroup_ownership(self):
+        pwn = PwnClient()
+        scope = 'hostgroup'
+        entity = self.test_entities[scope]
+        owners = self.test_ownership[1]
+        options = self.test_options
+        pwn.update_or_create_ownership(entity, scope, owners)
+
+    def test_04_pwn_get_hostgroup_ownership(self):
+        pwn = PwnClient()
+        scope = 'hostgroup'
+        entity = self.test_entities[scope]
+        expected_owners = pwn.clean_owners(self.test_ownership[1])
+        expected_options = self.test_options
+        result = pwn.get_ownership(entity, scope)
+        self.assertEqual(expected_owners, result['owners'])
+        self.assertEqual(expected_options, result['options'])
+
+    def test_05_pwn_del_hostgroup_ownership(self):
+        pwn = PwnClient()
+        scope = 'hostgroup'
+        entity = self.test_entities[scope]
+        result = pwn.delete_ownership(entity, scope)
+        self.assertRaises(AiToolsPwnNotFoundError, pwn.get_ownership, entity, scope)
+
+    # Now the sama with modules
+
+    def test_11_pwn_add_module_ownership(self):
+        pwn = PwnClient()
+        scope = 'module'
+        entity = self.test_entities[scope]
+        owners = self.test_ownership[0]
+        options = self.test_options
+        pwn.update_or_create_ownership(entity, scope, owners, options=options)
+
+    def test_12_pwn_get_module_ownership(self):
+        pwn = PwnClient()
+        scope = 'module'
+        entity = self.test_entities[scope]
+        expected_owners = pwn.clean_owners(self.test_ownership[0])
+        expected_options = self.test_options
+        result = pwn.get_ownership(entity, scope)
+        self.assertEqual(expected_owners, result['owners'])
+        self.assertEqual(expected_options, result['options'])
+
+    def test_13_pwn_update_module_ownership(self):
+        pwn = PwnClient()
+        scope = 'module'
+        entity = self.test_entities[scope]
+        owners = self.test_ownership[1]
+        options = self.test_options
+        pwn.update_or_create_ownership(entity, scope, owners)
+
+    def test_14_pwn_get_module_ownership(self):
+        pwn = PwnClient()
+        scope = 'module'
+        entity = self.test_entities[scope]
+        expected_owners = pwn.clean_owners(self.test_ownership[1])
+        expected_options = self.test_options
+        result = pwn.get_ownership(entity, scope)
+        self.assertEqual(expected_owners, result['owners'])
+        self.assertEqual(expected_options, result['options'])
+
+    def test_15_pwn_del_module_ownership(self):
+        pwn = PwnClient()
+        scope = 'module'
+        entity = self.test_entities[scope]
+        result = pwn.delete_ownership(entity, scope)
+        self.assertRaises(AiToolsPwnNotFoundError, pwn.get_ownership, entity, scope)
