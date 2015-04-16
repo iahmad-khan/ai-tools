cb1d5ee6f61ff7d49baa3b93e196c59fd2211d94
Alberto Peon <alberto.rodriguez.peon@cern.ch>
2015-04-16 15:27:02 +0200

    Include a few functional tests for ai-rc

---
+++ t_functional/airc.sh
@@ -0,0 +1,42 @@
+# This is a not very nice bash script to do some kind
+# of naive functional testing for ai-add-param
+
+CONF=$(mktemp)
+cat > $CONF << EOF
+
+[DEFAULT]
+dereference_alias = true
+
+[pdb]
+pdb_hostname = judy.cern.ch
+pdb_port = 9081
+pdb_timeout = 15
+EOF
+
+function _expect {
+  ret=$1
+  shift
+  cmd=$*
+  eval $cmd
+  if [ $ret -ne $? ]
+  then
+    echo "$cmd didn't return the expected return code ($ret)"
+    exit
+  fi
+}
+
+echo "Wrong combinations of arguments"
+_expect 2 ai-rc --same-project-as lxplusfake001 "LXBATCH"
+
+echo "Machine not in DNS"
+_expect 1 ai-rc --same-project-as lxplusfake001
+
+echo "Happy path"
+_expect 0 ai-rc
+_expect 0 ai-rc --same-project-as lxplus
+_expect 0 ai-rc -s lxplus
+_expect 0 ai-rc "LXPLUS"
+
+echo "Tearing down..."
+rm -f $CONF
+echo "All tests passed :)"
